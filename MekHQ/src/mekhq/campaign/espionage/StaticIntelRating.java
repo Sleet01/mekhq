bb40576de31783a65cc367331723eab9aabacbc4
sleet01 <sleet01@hotmail.com>
2025-12-28 17:17:50 -0800

    Add StaticIntelRating for bots, and start on SphereOfInfluence

---
+++ MekHQ/src/mekhq/campaign/espionage/StaticIntelRating.java
@@ -0,0 +1,62 @@
+package mekhq.campaign.espionage;
+
+/**
+ * This class represents an IntelRating that does not age or improve.
+ * However, its levels may be changed in some circumstances.
+ */
+public class StaticIntelRating extends IntelRating {
+
+    public StaticIntelRating() {
+        super();
+    }
+
+    public StaticIntelRating(int rating) {
+        super(rating);
+    }
+
+    public StaticIntelRating(int forcesLevel, int positionLevel, int logisticsLevel,
+          int personnelLevel, int commsLevel, int financialLevel, int localLevel, int counterLevel) {
+        super(
+              forcesLevel,
+              positionLevel,
+              logisticsLevel,
+              personnelLevel,
+              commsLevel,
+              financialLevel,
+              localLevel,
+              counterLevel
+        );
+    }
+
+    @Override
+    public void ageAllIntelByOne() {
+
+    }
+
+
+    @Override
+    public void setAnIntelToLevel(String name, int level) {
+
+    }
+
+    @Override
+    public void setAllIntelToLevel(int level) {
+
+    }
+
+    @Override
+    public void ageAllIntel(int delta) {
+
+    }
+
+    @Override
+    public void ageAnIntel(String name, int delta) {
+
+    }
+
+    @Override
+    public void improveAnIntel(String name, int delta) {
+
+    }
+}
+
