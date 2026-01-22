ddee32871859b5d5cf2315e78aa7a6f3ab75411a
sleet01 <sleet01@hotmail.com>
2026-01-22 15:25:47 -0800

    Change default rating to 12 for test purposes

--- MekHQ/src/mekhq/gui/view/AtBScenarioViewPanel.java
+++ MekHQ/src/mekhq/gui/view/AtBScenarioViewPanel.java
@@ -499,7 +499,7 @@ public class AtBScenarioViewPanel extends JScrollablePanel {
             if (useEspionage && (team != playerTeam)) {
                 // Espionage-style information obscuring, but only for enemies
                 // We _should_ have a rating for every foe in this scenario, which means every enemy bot _team_
-                IntelRating rating = (soi != null) ? soi.getActorRatingForFoe(playerTeam, team) : new IntelRating(0);
+                IntelRating rating = (soi != null) ? soi.getActorRatingForFoe(playerTeam, team) : new IntelRating(12);
                 for (String entityString : allEntries) {
                     int unitIndex = allEntries.indexOf(entityString);
                     Entity entity = curScenario.getBotForce(i).getFullEntityList(curCampaign).get(unitIndex);
